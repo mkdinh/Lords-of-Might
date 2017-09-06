54c77a0d649c7c81959c8743b4c346961664b53a
Kurt Griarte <kurtgee11@gmail.com>
2017-09-06 16:04:50 -0400

    update

--- app/routes/html_controller.js
+++ app/routes/html_controller.js
@@ -9,14 +9,22 @@ router.get('/', (req, res) => {
 })
 
 router.get('/signUp', function (req, res) {
-    res.render('signUp', {})
+    res.render('signUp')
 })
 
-router.get('/sprite', (req,res) => {
+router.get('/userPage', function (req, res) {
+    res.render('userPage')
+})
+
+router.get('/shop', function (req, res) {
+    res.render('shop')
+})
+
+router.get('/sprite', (req, res) => {
     res.render('sprite')
 })
 
-router.get('/game', (req,res) => {
+router.get('/game', (req, res) => {
 
     var isLoggedIn = !!req.user;
     res.render('game', {
