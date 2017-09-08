e42acf788fb53e44c9052f993769f92445ff3120
Michael Dinh <mkdinh94@gmail.com>
2017-09-08 16:33:11 -0400

    remove extra files

--- app/public/img/map/MageCity.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="MageCity" tilewidth="32" tileheight="32" tilecount="360" columns="8">
- <image source="magecity.png" width="256" height="1450"/>
-</tileset>
--- app/public/img/map/town2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesheet" tilewidth="32" tileheight="32" tilecount="1960" columns="20">
- <image source="tilesheet.png" width="640" height="3136"/>
-</tileset>
