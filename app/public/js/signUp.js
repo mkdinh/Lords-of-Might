24549b95d4155be8dceb6e7214b836d8254ee0fe
kurtgee6 <kurtgee11@gmail.com>
2017-09-13 13:24:32 -0400

    small change

--- app/public/js/signUp.js
+++ app/public/js/signUp.js
@@ -28,140 +28,208 @@ var female = [
     }
 ];
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-//once user clicks the next button
+    //once user clicks the next button
 
-$("#button").click(function () {
-    
-    //removes initial divs and tags
-    $( "label" ).remove();
-    $( "#createName" ).remove();
-    $( "#createPass" ).remove();
-    $( "#createNickname" ).remove();
-    $( "#createEmail" ).remove();
-    $(".three").remove();
-    
-    //creating new divs and ids
-    $( "#signup-wrapper" ).append( "<div id=sexContainer>" + "<p>CHOOSE YOUR CHARACTER PROFILE" + "</p>"+
-                                    "<input id=male type=submit value=MALE >OR " + 
-                                    "<input id=female type=submit value=FEMALE >" + "<div id=picContainer>" +
-                                    "<div id=picture>" + "</div>" +"</div>");
-    
-    
-      //once user clicks the male button
-     $("#male").click(function(){
-        
-        $( "#picture" ).empty();
-         
-        $('#picture').prepend('<img id="picFour" src=' + male[3].image  +' />')        
-        $('#picture').prepend('<img id="picThree" src=' + male[2].image  +' />')
-        $('#picture').prepend('<img id="picTwo" src=' + male[1].image  +' />')
-        $('#picture').prepend('<img id="picOne" src=' + male[0].image  +' />')
-         
-         
-        $("#picOne").click(function(){
-            console.log("Pic One male");
-        });
-        $("#picTwo").click(function(){
-            console.log("Pic Two male");
-        });
-        $("#picThree").click(function(){
-            console.log("Pic Three male");
-        });
-        $("#picFour").click(function(){
-            console.log("Pic Four male");
-        });
-         
-         
-          if ($('#picContainer').contents().length !== 0) {
-               
-                  $("#female").click(function(){
-    
-                    $( "#picture" ).empty();
+    $("#button").click(function () {
 
-                    $('#picture').prepend('<img id="picFour" src=' + female[3].image  +' />')        
-                    $('#picture').prepend('<img id="picThree" src=' + female[2].image  +' />')
-                    $('#picture').prepend('<img id="picTwo" src=' + female[1].image  +' />')
-                    $('#picture').prepend('<img id="picOne" src=' + female[0].image  +' />')
+        //removes initial divs and tags
+        $("label").remove();
+        $("#createName").remove();
+        $("#createPass").remove();
+        $("#createNickname").remove();
+        $("#createEmail").remove();
+        $(".three").remove();
+
+        //creating new divs and ids
+        $("#signup-wrapper").append("<div id=sexContainer>" + "<p>CHOOSE YOUR CHARACTER PROFILE" + "</p>" +
+            "<input id=male type=submit value=MALE >OR " +
+            "<input id=female type=submit value=FEMALE >" + "<div id=picContainer>" +
+            "<div id=picture>" + "</div>" + "</div>");
 
 
-                    $("#picOne").click(function(){
-                        console.log("Pic One female");
+        //once user clicks the male button
+        $("#male").click(function () {
+
+            $("#picture").empty();
+
+            $('#picture').prepend('<img id="picFour" src=' + male[3].image + ' />')
+            $('#picture').prepend('<img id="picThree" src=' + male[2].image + ' />')
+            $('#picture').prepend('<img id="picTwo" src=' + male[1].image + ' />')
+            $('#picture').prepend('<img id="picOne" src=' + male[0].image + ' />')
+
+
+            $("#picOne").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+            });
+            $("#picTwo").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+            });
+            $("#picThree").click(function () {
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+            });
+            $("#picFour").click(function () {
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+            });
+
+
+            if ($('#picContainer').contents().length !== 0) {
+
+                $("#female").click(function () {
+
+                    $("#picture").empty();
+
+                    $('#picture').prepend('<img id="picFour" src=' + female[3].image + ' />')
+                    $('#picture').prepend('<img id="picThree" src=' + female[2].image + ' />')
+                    $('#picture').prepend('<img id="picTwo" src=' + female[1].image + ' />')
+                    $('#picture').prepend('<img id="picOne" src=' + female[0].image + ' />')
+
+
+                    $("#picOne").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
                     });
-                    $("#picTwo").click(function(){
-                        console.log("Pic Two female");
+                    $("#picTwo").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
                     });
-                    $("#picThree").click(function(){
-                        console.log("Pic Three female");
+                    $("#picThree").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
                     });
-                    $("#picFour").click(function(){
-                        console.log("Pic Four female");
+                    $("#picFour").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
                     });
 
-                });
-              
-            }
-         
- 
-    });
-    
-    //once user clicks the female button
-     $("#female").click(function(){
-        
-        $( "#picture" ).empty();
-
-        $('#picture').prepend('<img id="picFour" src=' + female[3].image  +' />')        
-        $('#picture').prepend('<img id="picThree" src=' + female[2].image  +' />')
-        $('#picture').prepend('<img id="picTwo" src=' + female[1].image  +' />')
-        $('#picture').prepend('<img id="picOne" src=' + female[0].image  +' />')
-        
-        $("#picOne").click(function(){
-            console.log("Pic One female");
-        });
-        $("#picTwo").click(function(){
-            console.log("Pic Two female");
-        });
-        $("#picThree").click(function(){
-            console.log("Pic Three female");
-        });
-        $("#picFour").click(function(){
-            console.log("Pic Four female");
-        });
-         
-          if ($('#picContainer').contents().length !== 0) {
-               
-                  $("#male").click(function(){
-
-                    $( "#picture" ).empty();
-
-                    $('#picture').prepend('<img id="picFour" src=' + male[3].image  +' />')        
-                    $('#picture').prepend('<img id="picThree" src=' + male[2].image  +' />')
-                    $('#picture').prepend('<img id="picTwo" src=' + male[1].image  +' />')
-                    $('#picture').prepend('<img id="picOne" src=' + male[0].image  +' />')
 
 
-                    $("#picOne").click(function(){
-                        console.log("Pic One male");
-                    });
-                    $("#picTwo").click(function(){
-                        console.log("Pic Two male");
-                    });
-                    $("#picThree").click(function(){
-                        console.log("Pic Three male");
-                    });
-                    $("#picFour").click(function(){
-                        console.log("Pic Four male");
+                    //hide 
+
+                    $("#submitProfile").click(function () {
+
+                        console.log('yo');
+
                     });
 
                 });
-              
-            }
-         
-    });
-          
-        
-});
-    
-});
 
+            }
+
+        });
+
+        //once user clicks the female button
+        $("#female").click(function () {
+
+            $("#picture").empty();
+
+            $('#picture').prepend('<img id="picFour" src=' + female[3].image + ' />')
+            $('#picture').prepend('<img id="picThree" src=' + female[2].image + ' />')
+            $('#picture').prepend('<img id="picTwo" src=' + female[1].image + ' />')
+            $('#picture').prepend('<img id="picOne" src=' + female[0].image + ' />')
+
+            $("#picOne").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+            });
+            $("#picTwo").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+            });
+            $("#picThree").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+            });
+            $("#picFour").click(function () {
+
+                $("#submitProfile").remove();
+
+                $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+            });
+
+            if ($('#picContainer').contents().length !== 0) {
+
+                $("#male").click(function () {
+
+                    $("#picture").empty();
+
+                    $('#picture').prepend('<img id="picFour" src=' + male[3].image + ' />')
+                    $('#picture').prepend('<img id="picThree" src=' + male[2].image + ' />')
+                    $('#picture').prepend('<img id="picTwo" src=' + male[1].image + ' />')
+                    $('#picture').prepend('<img id="picOne" src=' + male[0].image + ' />')
+
+
+                    $("#picOne").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+                    });
+                    $("#picTwo").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+                    });
+                    $("#picThree").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+                    });
+                    $("#picFour").click(function () {
+
+                        $("#submitProfile").remove();
+
+                        $("#signup-wrapper").append("<input id=submitProfile type=submit value=Ready? />");
+
+                    });
+
+                });
+
+            }
+
+        });
+
+
+    });
+
+
+
+});
